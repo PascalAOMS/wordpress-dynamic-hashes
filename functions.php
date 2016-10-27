bc17b831b77cc5b51f2fd7fb114d7bf835f191b4
PascalAOMS <email@artofmyself.com>
2016-10-27 11:55:25 +0200

    added CSS loop

--- functions.php
+++ functions.php
@@ -1,8 +1,31 @@
 <?php
 
 function enqueue_files() {
-  $dirJS = new DirectoryIterator(get_stylesheet_directory() . '/js');
+  $dirCSS = new DirectoryIterator(get_stylesheet_directory() . '/css');
+  $dirJS  = new DirectoryIterator(get_stylesheet_directory() . '/js');
 
+  // CSS
+  foreach ($dirCSS as $file) {
+
+    if (pathinfo($file, PATHINFO_EXTENSION) === 'css') {
+      $fullName = basename($file);
+      $name = substr(basename($fullName), 0, strpos(basename($fullName), '.'));
+
+      switch($name) {
+
+          default:
+              $deps = null;
+              break;
+
+      }
+
+      wp_enqueue_script( $name, get_template_directory_uri() . '/css/' . $fullName, $deps, null, true );
+
+    }
+
+  }
+
+  // JS
   foreach ($dirJS as $file) {
 
     if (pathinfo($file, PATHINFO_EXTENSION) === 'js') {
